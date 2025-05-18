1a2f607246efc01a86b66eeb86100c4775b2731a
Aaron <aaronperezaguilera@gmail.com>
2025-05-18 18:10:06 +0200

    feat: Add loading component for forum question page with skeleton elements

---
+++ src/app/forum/[id]/loading.tsx
@@ -0,0 +1,43 @@
+import { SnippetCardSkeleton } from "@/components/skeletons/snippet-card-skeleton";
+import { Button } from "@/components/ui/button";
+import { Skeleton } from "@/components/ui/skeleton";
+
+export default function Loading() {
+  return (
+    <main className="container mx-auto mt-16 flex flex-col gap-4 min-h-screen">
+      <div className="flex items-center">
+        <Button
+          variant="ghost"
+          className={`pr-4 pl-2 py-2 h-full -translate-x-2 w-fit z-50`}
+        >
+          <div className="flex gap-3 items-center">
+            <Skeleton className="h-12 w-12" />
+            <div className="flex flex-col gap-2">
+              <div className="flex gap-2">
+                <Skeleton className="w-12 h-4" />
+                <Skeleton className="w-12 h-4" />
+              </div>
+              <span>
+                <Skeleton className="w-12 h-4" />
+              </span>
+            </div>
+          </div>
+        </Button>
+      </div>
+      <div className="flex flex-col gap-4 pb-4 border-b">
+        <div className="flex flex-col gap-4">
+          <Skeleton className="h-7 w-1/2" />
+          <Skeleton className="h-14 w-full" />
+        </div>
+        <Skeleton className="h-24 w-full" />
+      </div>
+      <div className="flex justify-end">
+        <Button type="button">Answer this question</Button>
+      </div>
+      <h2 className="text-xl">Answers</h2>
+      <SnippetCardSkeleton />
+      <SnippetCardSkeleton />
+      <SnippetCardSkeleton />
+    </main>
+  );
+}
