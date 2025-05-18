a1ca979d2bf692c4c71af8af39aed4239bfefd66
Aaron <aaronperezaguilera@gmail.com>
2025-05-18 18:14:55 +0200

    feat: Update MainLayout to fetch current user asynchronously

--- src/app/(mainLayout)/layout.tsx
+++ src/app/(mainLayout)/layout.tsx
@@ -1,10 +1,15 @@
 import { SnippetsWidget } from "@/components/snippets-widget";
+import { currentUser } from "@clerk/nextjs/server";
 
-export default function MainLayout({
+export default async function MainLayout({
   children,
 }: {
   children: React.ReactNode;
 }) {
+  const user = await currentUser();
+  if (!user) {
+    return children;
+  }
   return (
     <main className="mt-16 grid grid-cols-[1.2fr_2.8fr_1.2fr] gap-16 relative min-h-screen">
       <SnippetsWidget />
